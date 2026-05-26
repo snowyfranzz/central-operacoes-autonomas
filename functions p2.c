9514c584049e35d5fcba8e019ee0e21a16463d11
RCalosena <gntapia11@gmail.com>
2026-05-26 09:52:56 -0300

    Create functions p2.c

---
+++ functions p2.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isNumber(char c) {
+    if (c >= 0x30 && c <= 0x39) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int isLetter(char c) {
+    if (toupper(c) >= 0x41 && toupper(c) <= 0x5A) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void readString(char s[], int size) {
+    printf("Escreva ate %d caracteres: ", size - 1);
+    fgets(s, size, stdin);
+    s[strcspn(s,"\n")] = '\0';
+}
+
+int verificar(char s[], int size) {
+    int i = 0, success = 0;
+
+    while (s[i] != '\0') {
+        if (i <= 1) {
+            if (isLetter(s[i])) {
+                success++;
+            }
+        } else {
+            if (isNumber(s[i])) {
+                success++;
+            }
+        }
+        i++;
+    }
+
+    if (success == size - 1) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    char gok[5];
+    int i, L = 0, N = 0;
+
+    readString(gok, sizeof(gok));
+
+    while (gok[i] != '\0') {
+        if (isLetter(gok[i])) {
+            L++;
+        }
+        if (isNumber(gok[i])) {
+            N++;
+        }
+
+        i++;
+    }
+
+    printf("\n%d numbers\n%d letters", N, L);
+
+    if (verificar(gok, sizeof(gok))) {
+        printf("\n\nID Compativel");
+    } else {
+        printf("\n\nID Incompativel");
+    }
+
+    return 0;
+}
