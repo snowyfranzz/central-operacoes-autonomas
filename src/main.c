5d26236d838955d4c775b32b27cf9662cdb2fc59
RCalosena <gntapia11@gmail.com>
2026-05-27 08:42:15 -0300

    comentado

--- src/main.c
+++ src/main.c
@@ -22,17 +22,20 @@ void limpaBuffer() {
     while (c = getchar() != '\n' && c != EOF);
 }
 
+//leia o input do usuario
 void readString(char s[], int size) {
     fgets(s, size, stdin);
     limpaBuffer();
     s[strcspn(s,"\n")] = '\0';
 }
 
+//leia um valor inteiro do usuario
 void readInt(int *num) {
     scanf("%d", num);
     limpaBuffer();
 }
 
+//verifica se o caracter eh um numero
 int isNumber(char c) {
     if (c >= 0x30 && c <= 0x39) {
         return 1;
@@ -41,6 +44,7 @@ int isNumber(char c) {
     return 0;
 }
 
+//verifica se o caracter eh uma letra
 int isLetter(char c) {
     if (toupper(c) >= 0x41 && toupper(c) <= 0x5A) {
         return 1;
@@ -48,9 +52,11 @@ int isLetter(char c) {
     return 0;
 }
 
+//verifica o input (se o input foi valido), funciona para strings numericos, strings de 1 letra e 2 digitos, e strings de 2 letras e 2 digitos
 int verificar(char s[], int size) {
     int i, success = 0, numOnly = 1;
 
+    //verifica se eh um string numerico
     for (i = 0; i < strlen(s); i++) {
         if (isLetter(s[i])) {
             numOnly = 0;
@@ -58,8 +64,9 @@ int verificar(char s[], int size) {
         }
     }
 
-    size--;
+    size--; //tira o '\0' do tamanho total
 
+    //se estiver na primeira metade, verifica se eh letra, se não, verifica se eh numero
     for (i = 0; i < strlen(s); i++) {
         if (i <= (size / 2) - 1 && !numOnly) {
             if (isLetter(s[i])) {
@@ -72,10 +79,12 @@ int verificar(char s[], int size) {
         }
     }
 
+    //se tem a quantidade certa de letras e numeros, a funcao eh verdadeira
     if (success == size) {
         return 1;
     }
 
+    //se nao, a funcao eh falsa
     return 0;
 }
 
@@ -89,32 +98,37 @@ int main() {
 
     setlocale(LC_ALL, "portuguese");
 
+    //pega  guarda o ID do usuario
     do {
+        //le um string
         system("cls");
         printf("Escreva o ID do equipamento: ");
         readString(equipamentoTemp.ID, sizeof(equipamentoTemp.ID));
 
+        //verifica se for valido
         if (verificar(equipamentoTemp.ID, sizeof(equipamentoTemp.ID))) {
             strcpy(equipamento.ID, equipamentoTemp.ID);
         } else {
-            printf("\nID Inv�lido\n\n");
+            printf("\nID Inválido\n\n");
             system("pause");
         }
     } while (!verificar(equipamentoTemp.ID, sizeof(equipamentoTemp.ID)));
 
-
+    //pega e guarda o tipo de equipamento
     do {
+        //le numero inteiro
         input = 0;
 
         system("cls");
         printf("Escolha o tipo de equipamento: \n\n\t");
         printf("1: AGV\n\t");
-        printf("2: Bra�o-Articulado\n\t");
+        printf("2: Braço-Articulado\n\t");
         printf("3: Esteira\n\t");
         printf("4: sorter\n\t");
         printf("5: transelevador\n\n> ");
         readInt(&input);
 
+        //verifica se eh valido
         switch(input) {
             case 1:
                 strcpy(equipamento.tipo, "AGV");
@@ -138,29 +152,35 @@ int main() {
         }
     } while (input < 1 || input > 5);
 
+    //pega e guarda o setor
     do {
+        //le string
         system("cls");
         printf("Escreva o Setor Associado ao equipamento: ");
         readString(equipamentoTemp.setorAssociado, sizeof(equipamentoTemp.setorAssociado));
 
+        //verifica se eh valido
         if (verificar(equipamentoTemp.setorAssociado, sizeof(equipamentoTemp.setorAssociado))) {
             strcpy(equipamento.setorAssociado, equipamentoTemp.setorAssociado);
         } else {
-            printf("\nID Inv�lido\n\n");
+            printf("\nID Inválido\n\n");
             system("pause");
         }
     } while (!verificar(equipamentoTemp.setorAssociado, sizeof(equipamentoTemp.setorAssociado)));
 
+    //pega e guarda o estado
     do {
+        //le numero inteiro
         input = 0;
 
         system("cls");
-        printf("Qual � o Estado Operacional atual do equipamento? \n\n\t");
+        printf("Qual é o Estado Operacional atual do equipamento? \n\n\t");
         printf("1: Ativo\n\t");
         printf("2: Inativo\n\t");
-        printf("3: Manuten��o\n\n> ");
+        printf("3: Manutenção\n\n> ");
         readInt(&input);
 
+        //verifica o numero
         switch(input) {
             case 1:
                 strcpy(equipamento.estado, "ATIVO");
@@ -178,29 +198,35 @@ int main() {
         }
     } while (input < 1 || input > 3);
 
+    //pega e guarda o ID do operador
     do {
+        //le string
         system("cls");
         printf("Escreva o ID do Operador do equipamento: ");
         readString(equipamentoTemp.IDo, sizeof(equipamentoTemp.IDo));
 
+        //verifica se eh valido
         if (verificar(equipamentoTemp.IDo, sizeof(equipamentoTemp.IDo))) {
             strcpy(equipamento.IDo, equipamentoTemp.IDo);
         } else {
-            printf("\nID Inv�lido\n\n");
+            printf("\nID Inválido\n\n");
             system("pause");
         }
     } while (!verificar(equipamentoTemp.IDo, sizeof(equipamentoTemp.IDo)));
 
+    //pega e guarda a prioridade
     do {
+        //le numero inteiro
         input = 0;
 
         system("cls");
-        printf("Qual � a Import�ncia (N�vel de Prioridade) do equipamento? \n\n\t");
+        printf("Qual é a Importáncia (Nível de Prioridade) do equipamento? \n\n\t");
         printf("1: Baixa\n\t");
-        printf("2: M�dia\n\t");
+        printf("2: Média\n\t");
         printf("3: Alta\n\n> ");
         readInt(&input);
 
+        //verifica se eh valido
         switch(input) {
             case 1:
                 strcpy(equipamento.prioridade, "BAIXA");
@@ -218,6 +244,7 @@ int main() {
         }
     } while (input < 1 || input > 3);
 
+    //imprime o resultado final
     system("cls");
     printf("Registro:\n\n\tID: %s\n\tTipo: %s\n\tSetor Associado: %s\n\tEstado Operacional: %s\n\tID do Operador: %s\n\tPrioridade: %s\n", equipamento.ID, equipamento.tipo, equipamento.setorAssociado, equipamento.estado, equipamento.IDo, equipamento.prioridade);
 
